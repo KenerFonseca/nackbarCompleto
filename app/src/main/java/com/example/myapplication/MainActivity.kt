ca143f8e2c96abfb16dfd2246adabe71dcd270e9
Kener J. Fonseca Jarquin <2010101@lab.ing.edu.ni>
2023-10-07 14:33:32 -0600

    snackbarKener

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+   lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+       var binding =  ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+    var btntoast = findViewById<Button>(R.id.btntoast)
+
+
+
+
+        binding.btntoast.setOnClickListener{
+            Toast.makeText(this, "hola soy toast", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnmostrar.setOnClickListener {
+            Snackbar.make(it, "hola soy nackbar",Snackbar.LENGTH_SHORT).show()
+        }
+
+    }
+}
